51064ab78eae4f17798870ce5bbe3fad4e0188c9
Manel <manel.aguilera91@gmail.com>
2022-01-27 20:35:10 +0100

    provando integración continua

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
         <>
         <div className="font-black">
             <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-                    Seguimiento Pacientes {""}
+                    Seguimiento Pacientes {""} Git Hub {""}
             <span className="text-indigo-600">Veterinaria</span>
             </h1> 
          </div>
